bcbb4b5e74c143185ed3517d5afa2ffd856dbe1e
lee <497232807@qq.com>
2026-07-27 11:43:44 +0800

    refactor(GameUIConfig): 移除BaseUID合并及相关注释

--- assets/lobby/main/bundle/script/config/GameUIConfig.ts
+++ assets/lobby/main/bundle/script/config/GameUIConfig.ts
@@ -3,9 +3,6 @@ import { UILayer } from "../../../../../script/XKit/GUI/UILayer";
 
 
 export enum UID {
-
-    //合并BaseUID
-    
     /**登录 */
     Login = 1000,
     /**大厅 */
@@ -17,8 +14,7 @@ export enum UID {
  * 游戏内的ui配置
  */
 export var GameUIConfig: { [key: number]: UIConfig } = {
-    //合并基础UI配置
-    ...BaseGameUIConfig,
+
     [UID.Login]: { layer: UILayer.UI, prefab: "prefabs/login", bundle: BundleConfig.lobby.toString()},
     [UID.Lobby]: { layer: UILayer.UI, prefab: "prefabs/lobby", bundle: BundleConfig.lobby.toString()}
 }
